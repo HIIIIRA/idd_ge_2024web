55d5c7f2668a0b28811f0c3788cdd9959a94a7bd
reo3190 <reo.taka3190@gmail.com>
2023-06-25 21:27:27 +0900

    test animation

--- assets/js/anim.js
+++ assets/js/anim.js
@@ -1,21 +1,19 @@
-var size = [
-  [ 0.66, 0.35],
-  [ 0.50, 0.67],
-  [ 0.48, 0.45],
-  [ 0.48, 0.69],
-  [ 0.23, 0.32]
-];
-
 var aspect = [];
 var Glyph;
+var pixelSize_PC = 200;
+var pixelSize_SP = 100;
+var pixelSize_rate;
+var mobile = false;
 
 $(document).ready(function () {
   Glyph = document.getElementsByClassName('glyph');
-
+  pixelSize_rate =  Math.pow(pixelSize_PC, 2) /  Math.pow(pixelSize_SP, 2);
+  console.log(pixelSize_rate);
   if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-    createPixel(100).then(animation);
+    mobile = true;
+    createPixel(pixelSize_SP).then(animation);
   } else {
-    createPixel(200).then(animation);
+    createPixel(pixelSize_PC).then(animation);
   }
 })
 
@@ -29,24 +27,22 @@ function createPixel(s) {
 
     Promise.all(imgPromises)
       .then(function (images) {
-        var t = [];
+        var t = new Array(Glyph.length).fill('');
         var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus fermentum libero sed ante molestie, et tincidunt libero ultrices. Phasellus sed placerat nunc, a condimentum nunc. Proin lobortis tincidunt accumsan. Vivamus vitae enim neque. Curabitur ante felis, sodales at enim id, commodo fermentum sapien. Donec faucibus risus vel ante ultricies commodo. Curabitur commodo quam quis enim porta auctor. Phasellus imperdiet imperdiet condimentum. Aliquam at magna orci. Quisque eget purus vel augue iaculis blandit at sed ex. In ultricies tincidunt lorem. Quisque vel massa placerat, maximus nulla dapibus, ullamcorper mi. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean a sollicitudin velit, non cursus lectus. Nulla vehicula cursus justo";
         var Count;
 
-        for (var i = 0; i < Glyph.length; i++) {
+        $(Glyph).each(function(i){
           var image = images[i];
           aspect[i] = image.height / image.width;
-
           var [w, h] = getGlyphSize(s, i);
           var ctx = createCanvas(w, h);
           ctx.drawImage(image, 0, 0, w, h);
 
           var imageData = ctx.getImageData(0, 0, w, h);
           var data = imageData.data;
-          t[i] = "";
           Count = 0;
 
-          for (let y = 0; y < h - 1; y++) {
+          for (let y = 0; y < h; y++) {
             let line = "";
             for (let x = 0; x < w; x++) {
               var index = (x + y * w) * 4;
@@ -72,7 +68,7 @@ function createPixel(s) {
           
           $(Glyph[i]).html(t[i]);
           $(Glyph[i]).fadeOut(0);
-        }
+        });
         resolve();
       });
   });
@@ -106,35 +102,51 @@ function animation(){
   startTime = new Date().getTime();
 
   var string = [];
-  var tex = [];
-  var c = [];
-  var fin = [];
+  var tex = new Array(Glyph.length).fill('');
+  var c = new Array(Glyph.length).fill(0);
+  var fin = new Array(Glyph.length).fill(false);
   var sp = 50;
   var reg = new RegExp(`.{1,${sp}}`, 'g'); 
 
+  if(mobile){
+    sp = Math.round(sp / pixelSize_rate);
+  }
+
+  var str = [];
+  // var t = [];
+
   for(let i = 0; i < Glyph.length; i++){
+    var st = [];
     string[i] = $(Glyph[i]).html().match(reg);
+
     $(Glyph[i]).html("");
     $(Glyph[i]).fadeIn(0);
-    
-    tex[i] = "";
-    c[i] = 0;
-    fin[i] = false;
   }
 
   function animationLoop(){
     $(Glyph).each(function(i){
+      if(i == 0){
+
+      }else if(c[i - 1] < string[i].length / 4){
+        c[i] = 0;
+        return true;
+      }
+
       if(string[i].length <= [c[i]]){
         fin[i] = true;
         return true;
       }
+
       var addTex = string[i][c[i]];
       addTex = addTex.replace(/凹/g,"&nbsp;").replace(/凸/g,"<br>");
       tex[i] += addTex;
-      c[i] ++;
+      //tex[i] += str[i][c[i]];
+
       $(Glyph[i]).html(tex[i]);
-    }); 
-    
+
+      c[i] ++;
+    });
+
     frameCount ++;
     endTime = new Date().getTime();
     if(endTime - startTime >= 1000){
@@ -153,14 +165,17 @@ function animation(){
     }
     
   }
-  animationLoop();
+  $(this).delay(4000).queue(function(){
+    animationLoop();
+  });
+  
 }
 
 $(window).resize(function(){
-  if (navigator.userAgent.match(/iPhone|Android.+Mobile/)) {
-    ReSize(100);
+  if (mobile) {
+    ReSize(pixelSize_SP);
   } else {
-    ReSize(200);
+    ReSize(pixelSize_PC);
   }
 });
 
@@ -172,13 +187,15 @@ function ReSize(s){
 
 const getGlyphSize = function(s, i){
   var titleWidth = $('#container_title').width();
-  var w = Math.round(size[i][0] * s);
+  var [sizeW,sizeH] = size(Glyph[i].id.replace("img_", ""));
+  var w = Math.round(sizeW * s);
   var h = Math.round(w * aspect[i]);
-  var {fontW, fontH} = getFontSize();
-  var ad = Math.pow((1 - (1 / s)), 2);
+  var [fontW, fontH] = getFontSize();
+  //var ad = Math.pow((1 - (1 / s)), 2);
+  var ad = 1;
 
-  var scaleW = (size[i][0] * titleWidth / (fontW * w)) * 100 * ad;
-  var scaleH = (size[i][1] * titleWidth / (fontH * h)) * 100 * ad;
+  var scaleW = (sizeW * titleWidth / (fontW * w)) * 100 * ad;
+  var scaleH = (sizeH * titleWidth / (fontH * h)) * 100 * ad;
 
   $(Glyph[i]).css({ "transform": `scale(${scaleW}%,${scaleH}%)` });
 
@@ -207,5 +224,39 @@ const getFontSize = function(){
 
   span.parentElement.removeChild(span);
 
-  return {fontW: width, fontH: height};
+  return [width, height];
 }
+
+const size = function(id){
+  var w,h;
+  switch(id){
+    case "F":
+      [w,h] = [ 0.66, 0.36];
+    break;
+    case "r":
+      [w,h] = [ 0.50, 0.67];
+    break;
+    case "o":
+      [w,h] = [ 0.46, 0.45];
+    break;
+    case "m":
+      [w,h] = [ 0.48, 0.69];
+    break;
+    case "T":
+      [w,h] = [ 0.23, 0.32];
+    break;
+    default:
+      [w,h] = [0,0];
+    break;
+  }
+
+  return [w,h];
+}
+
+// var size = [
+//   [ 0.66, 0.36],
+//   [ 0.50, 0.67],
+//   [ 0.46, 0.45],
+//   [ 0.48, 0.69],
+//   [ 0.23, 0.32]
+// ];
